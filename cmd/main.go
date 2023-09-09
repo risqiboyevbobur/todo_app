8913659757ece9ae5a29a93e8d7dfd6999ef59b7
risqiboyevbobur <brisqiboyev00@gmail.com>
2023-09-09 18:48:22 +0500

    3-4-video completed successfully

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,28 @@ package main
 import (
 	"log"
 
-	"github.com/risqiboyevbobur/todo_app.git"
+	"github.com/spf13/viper"
+
+	todo "github.com/risqiboyevbobur/todo_app.git"
 	"github.com/risqiboyevbobur/todo_app.git/pkg/handler"
 	"github.com/risqiboyevbobur/todo_app.git/pkg/repository"
 	"github.com/risqiboyevbobur/todo_app.git/pkg/service"
 )
 
-func main()  {
+func main() {
+	if err := initConfigs(); err != nil{
+		log.Fatalf("error initilization configs %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000",handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error accured while running http server: %s", err.Error())
 	}
+}
+func initConfigs() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
