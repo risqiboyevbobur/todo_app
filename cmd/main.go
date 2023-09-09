c7fb4521302673847dee8f112ecb0d3354ce0a84
risqiboyevbobur <brisqiboyev00@gmail.com>
2023-09-09 17:32:07 +0500

    3-video completed successfully

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 
-	todo "github.com/risqiboyevbobur/todo_app.git"
+	"github.com/risqiboyevbobur/todo_app.git"
 	"github.com/risqiboyevbobur/todo_app.git/pkg/handler"
+	"github.com/risqiboyevbobur/todo_app.git/pkg/repository"
+	"github.com/risqiboyevbobur/todo_app.git/pkg/service"
 )
 
 func main()  {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
 	if err := srv.Run("8000",handlers.InitRoutes()); err != nil {
 		log.Fatalf("error accured while running http server: %s", err.Error())
